2634cf9f7640e24be685d7c59e11a06b44520397
ausahin <ali.sahin@epfl.ch>
2021-12-18 12:45:52 +0100

    Participant string representations simplified.

--- src/main/scala/dolevYao/Participant.scala
+++ src/main/scala/dolevYao/Participant.scala
@@ -3,11 +3,11 @@ package dolevYao
 sealed abstract class Participant
 
 object R extends Participant {
-  override def toString: String = "dolevYao.R"
+  override def toString: String = "R"
 }
 object S extends Participant {
-  override def toString: String = "dolevYao.S"
+  override def toString: String = "S"
 }
 object Z extends Participant {
-  override def toString: String = "dolevYao.Z"
+  override def toString: String = "Z"
 }
