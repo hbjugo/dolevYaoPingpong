16a4b5e423ffa4f768a5b661c84f15ffc3571199
ausahin <ali.sahin@epfl.ch>
2021-12-22 21:43:29 +0100

    Some comments.

--- src/main/scala/dolevYao/CollapsingRelation.scala
+++ src/main/scala/dolevYao/CollapsingRelation.scala
@@ -7,21 +7,24 @@ class CollapsingRelation(val n: Int) {
   val paths: mutable.Map[(Int, Int), List[Operator]] = mutable.Map.empty
   // Represents the membership.
   val membership: Array[Array[Boolean]] = Array.ofDim(n, n)
+  // Each possible self-loop is already a collapsing path. Add them.
   Range(0, n).foreach(i => {
     membership(i)(i) = true
     paths((i, i)) = List.empty
   })
 
+  // Returns whether (i, j) in C
   def in(i: Int, j: Int): Boolean = membership(i)(j)
+  // Returns the collapsing path from i to j iff (i, j) in C.
   def getPath(i: Int, j: Int): Option[List[Operator]] = paths.get((i, j))
 
-  // (i, j) in C and (j, k) in C already.
+  // We are given (i, j) in C and (j, k) in C already. Combines them.
   def extendRight(i: Int, j: Int, k: Int): Unit = {
     membership(i)(k) = true
     paths((i, k)) = paths((i, j)).appendedAll(paths((j, k)))
   }
 
-  // (i, j) in C and (k, i) in C already.
+  // We are given (i, j) in C and (k, i) in C already. Combines them.
   def extendLeft(i: Int, j: Int, k: Int): Unit = {
     membership(k)(j) = true
     paths((k, j)) = paths((k, i)).appendedAll(paths((i, j)))
